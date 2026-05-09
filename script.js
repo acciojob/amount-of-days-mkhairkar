25c792687c71f6143dbae957c2932f649ebe9f85
Mayur Khairkar <95740994+mkhairkar@users.noreply.github.com>
2026-05-09 15:37:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 //your JS code here. If required.
-function isSameType(value1, value2) {
-  
-  if (Number.isNaN(value1) || Number.isNaN(value2)) {
-    return Number.isNaN(value1) && Number.isNaN(value2);
+function daysOfAYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } else {
+    return 365;
   }
-
-  return typeof value1 === typeof value2;
 }
-
-// do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
-
-alert(isSameType(value1, value2));
